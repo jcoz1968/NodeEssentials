6903c428d0581a9124f99c7099356337adcbcf24
Jeffery Cosby <jcoz1968@gmail.com>
2019-05-18 13:35:36 -0500

    create a custom module

--- section4/lib/collectAnswers.js
+++ section4/lib/collectAnswers.js
@@ -1,17 +1,10 @@
 const readline = require("readline");
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const questions = [
-  "What is your name? ",
-  "Where do you live? ",
-  "What are you going to do with node js? "
-];
-
-const collectAnswers = (questions, done) => {
+module.exports = (questions, done = f => f) => {
   const answers = [];
   const [firstQuestion] = questions;
 
@@ -26,9 +19,3 @@ const collectAnswers = (questions, done) => {
 
   rl.question(firstQuestion, questionAnswered);
 };
-
-collectAnswers(questions, answers => {
-  console.log("Thank you for your answers. ");
-  console.log(answers);
-  process.exit();
-});
