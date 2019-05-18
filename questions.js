10c8d64c4614458498e7423e1cc8581a43c63d8b
Jeffery Cosby <jcoz1968@gmail.com>
2019-05-18 12:32:25 -0500

    standard input

--- questions.js
+++ questions.js
@@ -14,3 +14,26 @@ const ask = (i = 0) => {
 };
 
 ask();
+
+const answers = [];
+process.stdin.on('data', data => { 
+  // process.stdout.write(`\n\n ${data.toString().trim()} \n\n`);
+  // process.exit();
+  answers.push(data.toString().trim());
+  if (answers.length < questions.length) {
+    ask(answers.length);    
+  } else {
+    process.exit();
+  }
+});
+
+process.on('exit', () => {
+  const [name, activity, language] = answers;
+  console.log(`
+  
+  Thank you for your answers.
+
+  Go ${activity} ${name} you can write ${language} code later!!!
+  
+  `);
+});
